5b55ce5ac03db3762e192987b4b3b8af14d212e2
aryanxcode01 <ay1403107@gmail.com>
2026-05-31 20:45:15 +0530

    question one solved

---
+++ DAY-1/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0 ;
+    int n ;
+    cout <<"enter n :" ; 
+    cin >>n ;
+    for(int i = 0 ; i<= n ; i++){
+        sum+=i;
+    }
+    cout <<"sum of n natural number is :" << sum << endl;
+    return 0;
+}
