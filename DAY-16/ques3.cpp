3789d2adbcedd740678b6e567c9f17efba806163
aryanxcode01 <ay1403107@gmail.com>
2026-06-16 11:32:41 +0530

    question 3 solved

---
+++ DAY-16/ques3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[] = {2, 7, 11, 15};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 9;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                cout << "Pair found: "<< arr[i] << " " << arr[j] << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No pair found";
+    return 0;
+}
