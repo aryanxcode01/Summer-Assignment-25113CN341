0c7e2f25a391d34dd2134ff06e8eab890794dda6
aryanxcode01 <ay1403107@gmail.com>
2026-06-22 15:52:45 +0530

    question 3 solved

---
+++ DAY-22/ques3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str="yu naveda hai momo , ka keh reho";
+
+    int freq[256]={0};
+
+    for(int i = 0 ; i< str.length() ; i++){
+        freq[str[i]]++;
+    }
+
+    for(int i= 0 ; i< 256 ; i++){
+        if(freq[i]>0){
+            cout << char(i) << " : " << freq[i] << endl; 
+        }
+    }
+    return 0;
+}
