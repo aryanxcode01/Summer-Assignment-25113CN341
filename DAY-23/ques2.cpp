268bd929410b6b574f11fd89fb2a6786d594961f
aryanxcode01 <ay1403107@gmail.com>
2026-06-23 18:18:25 +0530

    question 2 solved

---
+++ DAY-23/ques2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string str = "aaabbcc";
+    string result = "";
+
+    int count = 1;
+
+    for (int i = 0; i < str.length(); i++){
+        while (i < str.length() - 1 && str[i] == str[i + 1]){
+            count++;
+            i++;
+        }
+
+        result += str[i];
+        result += to_string(count);
+        count = 1;
+    }
+    cout << "Compressed String: " << result;
+    return 0;
+}
